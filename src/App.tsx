aaedb93d6883964cfeeb8a4533ab37a9ed1efd64
Niklas Mårdby <niklas.mardby@gmail.com>
2024-04-10 13:20:43 +0200

    Demoed set width with state and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
-import "./App.css";
+import "./App.scss";
 
 // transition & animation
 // className och useState (animation + keyframes)
 // bara CSS? hover, active
 
 // style={ } inline styling i React
+
 // useEffect
 
 function App() {
 	const [chosen, setChosen] = useState(false);
+	const [stateWidth, setWidth] = useState(100);
+
 	return (
 		<>
 			<h1>moving-stuff</h1>
@@ -23,6 +26,19 @@ function App() {
 				New div
 			</div>
 			<div className={`new ${chosen && "newColor"}`}>Gorilla</div>
+			<hr />
+			<input
+				type="number"
+				name="width"
+				id="width"
+				value={stateWidth}
+				onChange={(e) => {
+					setWidth(Number(e.target.value));
+				}}
+			/>
+			<div className="width" style={{ width: stateWidth }}>
+				demo
+			</div>
 		</>
 	);
 }
