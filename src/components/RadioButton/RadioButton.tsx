54f37ec95e3461d1fc729f491537077c52ad0434
Niklas Mårdby <niklas.mardby@gmail.com>
2024-04-10 13:20:17 +0200

    Added radio button component

---
+++ src/components/RadioButton/RadioButton.tsx
@@ -0,0 +1,8 @@
+import "./RadioButton.scss";
+
+/* 
+Gör en radio button med en animerad hover-effekt. 
+Lägg till en till animation när man klickar på den och 
+ytterligare ett utseende när den är iklickad. 
+Med så mkt CSS som möjligt.
+*/
