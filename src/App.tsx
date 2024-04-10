352b8c911225fb40b979c085fefde69d3eb45582
Niklas Mårdby <niklas.mardby@gmail.com>
2024-04-10 10:56:18 +0200

    Demo animation del 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 // transition & animation
-// className och useState
-// style={ }
+// className och useState (animation + keyframes)
+// bara CSS? hover, active
+
+// style={ } inline styling i React
 // useEffect
 
 function App() {
+	const [chosen, setChosen] = useState(false);
 	return (
 		<>
 			<h1>moving-stuff</h1>
+			<div className="test">Test</div>
+			<div
+				className={`new`}
+				onClick={() => {
+					setChosen(!chosen);
+				}}
+			>
+				New div
+			</div>
+			<div className={`new ${chosen && "newColor"}`}>Gorilla</div>
 		</>
 	);
 }
